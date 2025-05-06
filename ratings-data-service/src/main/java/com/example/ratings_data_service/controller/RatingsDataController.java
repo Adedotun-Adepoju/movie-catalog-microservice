f3d3e1c82fa8041d2d44da694cdeae659729a6e4
Adedotun-Adepoju <d.e.adepoju@gmail.com>
2025-05-06 18:17:13 +0100

    implement ratings-data-service API

--- ratings-data-service/src/main/java/com/example/ratings_data_service/controller/RatingsDataController.java
+++ ratings-data-service/src/main/java/com/example/ratings_data_service/controller/RatingsDataController.java
@@ -1,2 +1,16 @@
-package com.example.ratings_data_service.controller;public class RatingsDataController {
+package com.example.ratings_data_service.controller;
+
+import com.example.ratings_data_service.models.Rating;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsDataController {
+  @GetMapping("/{movieId}")
+  public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+    return new Rating(movieId, 4);
+  }
 }
